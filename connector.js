3273b9c24465c8c51a433e15aa0697fc2f884716
virgilvox <moheeb.zara@citrix.com>
2016-03-03 11:06:20 -0700

    v3.1.9 switched to kryten

--- connector.js
+++ connector.js
@@ -68,7 +68,7 @@ Connector.prototype.onMessage = function(message){
 Connector.prototype.onReady = function(){
   var self = this;
   self.conx.whoami({uuid: self.config.uuid}, function(device){
-    self.plugin.setOptions(device.options || {});
+    self.plugin.setOptions(device || {});
     var oldRecentVersions = device.recentVersions || [];
     var recentVersions = _.union(oldRecentVersions, [packageJSON.version]);
     self.conx.update({
@@ -96,7 +96,6 @@ Connector.prototype.run = function(){
   self.plugin.on('error', self.emitError);
 
   self.plugin.on('update', function(properties){
-    self.emit('update', properties);
     self.conx.update(properties);
   });
 
